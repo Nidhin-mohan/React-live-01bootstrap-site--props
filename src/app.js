20ec50cda2d6e84dd05b2ad9a9965d606a5698a7
Nidhin-mohan <nidhinmohannidhin>
2022-10-14 21:26:02 +0530

    React begining

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './app.css'
+
+function App(){
+
+    return (
+      <>
+        <h2>This is app Function -Nidhin</h2>
+        <p>This is a paragraph</p>
+      </>
+    );
+}
+
+export default App
